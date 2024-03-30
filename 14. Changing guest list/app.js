d0cbc1128b87a71e1d14f8d54cd7391765ae449f
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-03-30 08:56:45 -0700

    Add files via upload

---
+++ 14. Changing guest list/app.js
@@ -0,0 +1,9 @@
+var guests = ["Alishba", "Faizan", "Adan"];
+console.log("I'd like to invite", guests[0], "to dinner.");
+console.log("I'd like to invite", guests[1], "to dinner.");
+console.log("I'd like to invite", guests[2], "to dinner.");
+console.log("Unfortunately", guests[2], "can't make it so I'm inviting Hammad to dinner.");
+var guests2 = ["Alishba", "Faizan", "Hammad"];
+console.log("I'm inviting", guests2[0], "to dinner.");
+console.log("I'm inviting", guests2[1], "to dinner.");
+console.log("I'm inviting", guests2[2], "to dinner.");
---
+++ 14. Changing guest list/app.ts
@@ -0,0 +1,13 @@
+let guests: string[] = ["Alishba", "Faizan", "Adan"]
+
+console.log("I'd like to invite", guests[0], "to dinner.");
+console.log("I'd like to invite", guests[1], "to dinner.");
+console.log("I'd like to invite", guests[2], "to dinner.");
+
+console.log("Unfortunately" ,guests[2], "can't make it so I'm inviting Hammad to dinner.");
+
+let guests2: string[] = ["Alishba", "Faizan", "Hammad"]
+
+console.log("I'm inviting", guests2[0], "to dinner.");
+console.log("I'm inviting", guests2[1], "to dinner.");
+console.log("I'm inviting", guests2[2], "to dinner.");
